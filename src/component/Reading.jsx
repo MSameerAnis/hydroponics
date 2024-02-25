f98d97a315a6c1e78e1a000bbcee67cd0d27b1db
Muhammad Sameer Anees <sameeranees1234@gmail.com>
2024-02-25 23:09:25 +0500

    fixing clear device id button

--- src/component/Reading.jsx
+++ src/component/Reading.jsx
@@ -129,7 +129,7 @@ const Reading = () => {
         
         {/* Clear button for device filter */}
         <button
-          className={`px-4 py-2 mb-2 ${
+          className={`px-4 py-2 mb-2 ml-2 ${
             deviceFilter !== '' ? 'mb-2' : '' 
           } bg-gray-300 text-gray-700`}
           onClick={() => setDeviceFilter('')}
